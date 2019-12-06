afeca3213813af2146748347b4ea2f8a793008a7
Matt R. Wilson <mwilson@shutterstock.com>
2019-12-06 08:13:33 -0700

    Notes from day 6.

--- 2019/day6.js
+++ 2019/day6.js
@@ -5,8 +5,17 @@ const readFile = require("../helpers/readFile");
 
 const input = readFile(__dirname, "./input/day6.txt");
 
+// Initial thoughts:
 // this was a shitty way to go, but my mind blanked on graph structures in the moment.
 // I never would have gone this route if I'd thought it through, I should have kept orbiting values in arrays.
+
+// Thoughts the next morning:
+// After a couple refactors, this ended up being not too bad given that the input isn't huge.
+// The big problem was, when trying to solve it quickly, having the graph backwards made it difficult
+// to wrap my head around what was happening on the root node during the counting. Specifically, I lost a lot of time
+// by having an off-by-one bug for all direct orbits expect for the one value that orbits the root (COM).
+// Refactoring the orbit map into two objects that track relationships and walk counts separately
+// allowed for a lot of simplifying everywhere else.
 const orbits = Object.fromEntries(input.map(line => line.split(")").reverse()));
 const counts = new DefaultDict(0);
 
@@ -38,6 +47,8 @@ const getPath = orbiting => {
     res.push(orbiting);
   }
 
+  // Reversing here is just to shorten the line length of the `while` comparison below.
+  // "Optimization" you say?
   return res.reverse();
 };
 
