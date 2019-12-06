4f5141cd820452020be5a6ae5d965fbc792c661f
Matt R. Wilson <mwilson@shutterstock.com>
2019-12-05 23:46:48 -0700

    Day six

---
+++ 2019/day6.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const lodash = require("lodash");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.resolve(__dirname, "./input/day6.txt"))
+  .toString()
+  .trim()
+  .split("\n");
+
+// this was a shitty way to go, but my mind blanked on graph structures in the moment.
+// I never would have gone this route if I'd thought it through, I should have kept orbiting values in arrays.
+const orbits = Object.fromEntries(input.map(line => line.split(")").reverse()));
+const counts = {};
+
+const incr = orbiting => {
+  if (orbiting === "COM") {
+    return;
+  }
+
+  const orbited = orbits[orbiting]; // what I wouldn't give for a defaultdict. (Proxy?)
+
+  if (counts[orbited]) {
+    counts[orbited]++;
+  } else {
+    counts[orbited] = 1;
+  }
+
+  incr(orbited); // silly rabbit
+};
+
+Object.keys(orbits).forEach(incr);
+
+const partOne = lodash.sum(Object.values(counts));
+console.log("part one", partOne); // 162816
+
+/**
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+ */
+const getPath = orbiting => {
+  const res = [];
+  while (orbiting !== "COM") {
+    orbiting = orbits[orbiting];
+    res.push(orbiting);
+  }
+
+  return res.reverse();
+};
+
+// Since the orbits are keyed by the orbiting values, it seemed faster to walk the two paths
+// down to the root, then compare how much of the paths were shared. A BFS probably wouldn't have
+// been any faster to write.
+const youPath = getPath("YOU");
+const sanPath = getPath("SAN");
+let shared = 0;
+
+while (youPath[shared] === sanPath[shared]) {
+  shared++;
+}
+
+const partTwo = youPath.length + sanPath.length - shared * 2;
+console.log("part two", partTwo); // 304
---
+++ 2019/day6.original.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const lodash = require("lodash");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.resolve(__dirname, "./input/day6.txt"))
+  .toString()
+  .trim()
+  .split("\n");
+
+const map = {
+  COM: {
+    // orbits: a,
+    count: 0
+  }
+};
+
+const incr = (i, x) => {
+  if (i === "COM") {
+    return x;
+  }
+  // console.log(i, map[i].orbits)
+  if (!map[i]) {
+    return x;
+  }
+
+  x++;
+  map[i].count++;
+  // if (map[i].orbits!=='COM') {
+  return incr(map[i].orbits, x);
+  // }
+};
+
+input.forEach(line => {
+  const [a, b] = line.split(")");
+
+  map[b] = {
+    orbits: a,
+    count: 1
+  };
+});
+
+Object.values(map).forEach(v => {
+  const x = incr(v.orbits, 0);
+  // console.log( v.orbits, x)
+});
+
+// console.log(map)
+const counts = Object.values(map).map(v => v.count);
+const partOne = lodash.sum(counts);
+console.log("part one", partOne); // 162816
+
+/**
+ *                           YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+ */
+const aaa = s => {
+  const res = [];
+  while (s !== "COM") {
+    res.push(map[s].orbits);
+    s = map[s].orbits;
+  }
+
+  return res;
+};
+// console.log(aaa("YOU"))
+// console.log(aaa("SAN"))
+const foo = aaa("YOU");
+const bar = aaa("SAN");
+
+while (foo[foo.length - 1] === bar[bar.length - 1]) {
+  foo.pop();
+  bar.pop();
+}
+// console.log(foo, bar)
+
+const partTwo = foo.length + bar.length;
+console.log("part two", partTwo); // 304
