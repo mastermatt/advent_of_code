9f0c243e45258eba0e3b530320384922402dcf7c
Matt R. Wilson <mwilson@shutterstock.com>
2019-12-07 23:04:58 -0700

    Day 8.

---
+++ 2019/day8.js
@@ -0,0 +1,30 @@
+const lodash = require("lodash");
+
+const readFile = require("../helpers/readFile");
+
+const input = readFile(__dirname, "./input/day8.txt")[0].split("");
+
+const horizontal = 25;
+const vertical = 6;
+const layerPixelCount = horizontal * vertical;
+
+const layers = lodash.chunk(input, layerPixelCount);
+const numZeros = layers.map(l => lodash.countBy(l)["0"]);
+const fewestIndex = numZeros.indexOf(Math.min(...numZeros));
+const counts = lodash.countBy(layers[fewestIndex]);
+
+const partOne = counts["1"] * counts["2"];
+console.log("part one", partOne); // 1703
+
+const transparent = "2";
+const finalImg = new Array(layerPixelCount).fill(transparent);
+
+layers.forEach(layer =>
+  layer
+    .filter((pixelColor, i) => finalImg[i] === transparent)
+    .forEach((pixelColor, i) => (finalImg[i] = pixelColor))
+);
+
+lodash.chunk(finalImg, horizontal).forEach(line => {
+  console.log(line.map(p => (p === "0" ? " " : "■")).join("")); // HCGFE
+});
---
+++ 2019/day8.original.js
@@ -0,0 +1,54 @@
+const lodash = require("lodash");
+
+const readFile = require("../helpers/readFile");
+
+const input = readFile(__dirname, "./input/day8.txt")[0]
+  .split("")
+  .map(Number);
+
+const h = 25;
+const v = 6;
+const layerSize = h * v;
+
+const count = (arr, x) => arr.filter(i => i === x).length;
+const layers = lodash.chunk(input, layerSize);
+const numZeros = layers.map(l => count(l, 0));
+
+let fewestZeros = Infinity;
+let fewestIndex;
+
+numZeros.forEach((num, i) => {
+  if (num < fewestZeros) {
+    fewestZeros = num;
+    fewestIndex = i;
+  }
+});
+
+const fewestLayer = layers[fewestIndex];
+const numOnes = count(fewestLayer, 1);
+const numTwos = count(fewestLayer, 2);
+console.log(numOnes, numTwos);
+
+const partOne = numOnes * numTwos;
+console.log("part one", partOne); // 1703
+
+const finalImg = new Array(layerSize).fill(2);
+
+layers.forEach(layer => {
+  layer.forEach((pixelColor, i) => {
+    if (pixelColor === 2 || finalImg[i] !== 2) {
+      return;
+    }
+
+    finalImg[i] = pixelColor;
+  });
+});
+
+const lines = lodash.chunk(finalImg, h);
+lines.forEach(line => {
+  console.log(line.map(p => (p === 0 ? " " : "■")).join(""));
+});
+
+// console.log(finalImg)
+const partTwo = 2;
+console.log("part two", partTwo); // ..
