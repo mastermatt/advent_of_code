65ea148df5924e0cc96cb1387d2b297b5474359a
Matt R. Wilson <mwilson@shutterstock.com>
2020-11-30 14:19:57 -0700

    Add missing 2018 Day 03.

---
+++ 2018/day03/index.js
@@ -0,0 +1,33 @@
+const lodash = require("lodash");
+const DefaultDict = require("../../helpers/defaultdict");
+const readFile = require("../../helpers/readFile");
+
+// #123 @ 3,2: 5x4
+const input = readFile(__dirname, "./input.txt").map(line =>
+  line
+    .match(/^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$/)
+    .slice(1, 6)
+    .map(digits => parseInt(digits))
+);
+
+const map = new DefaultDict(Array);
+
+input.forEach(([id, left, top, width, height]) => {
+  for (let x = left; x < left + width; ++x) {
+    for (let y = top; y < top + height; ++y) {
+      map[[x, y]].push(id);
+    }
+  }
+});
+
+const dups = Object.values(map).filter(arr => arr.length > 1);
+const partOne = dups.length;
+console.log("part one", partOne); // 113966
+
+const noOverlaps = lodash.difference(
+  input.map(([id]) => id),
+  ...dups
+);
+
+const partTwo = noOverlaps[0];
+console.log("part two", partTwo); // 235
