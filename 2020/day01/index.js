0ac777b4903b3cb1acedac6120865153ee4d1d05
Matt R. Wilson <mwilson@shutterstock.com>
2020-11-30 22:21:36 -0700

    2020 has begun. Lets do this!

---
+++ 2020/day01/index.js
@@ -0,0 +1,24 @@
+const Iter = require("es-iter");
+const readFile = require("../../helpers/readFile");
+
+const input = readFile(__dirname, "./input.txt").map(line => parseInt(line));
+
+let partOne;
+for (const [a, b] of new Iter(input).combinations(2)) {
+  if (a + b === 2020) {
+    partOne = a * b;
+    break;
+  }
+}
+
+console.log("part one", partOne); // 731731
+
+let partTwo;
+for (const [a, b, c] of new Iter(input).combinations(3)) {
+  if (a + b + c === 2020) {
+    partTwo = a * b * c;
+    break;
+  }
+}
+
+console.log("part two", partTwo); // 116115990
