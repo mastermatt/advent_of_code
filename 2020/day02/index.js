2af949d88ae7a27043f2aed9df478d9830821453
Matt R. Wilson <mwilson@shutterstock.com>
2020-12-01 22:42:15 -0700

    2020 Day 02

---
+++ 2020/day02/index.js
@@ -0,0 +1,26 @@
+const lodash = require("lodash");
+const readFile = require("../../helpers/readFile");
+
+const input = readFile(__dirname, "./input.txt");
+
+// 1-3 a: abcde
+const regexp = /^(?<first>\d+)-(?<second>\d+) (?<char>[a-z]): (?<password>[a-z]+)$/;
+const parsed = input.map(line => line.match(regexp).groups);
+
+const validPartOnes = parsed.filter(({ first, second, char, password }) => {
+  const count = lodash.countBy(password)[char];
+  return count >= first && count <= second;
+});
+
+const partOne = validPartOnes.length;
+console.log("part one", partOne); // 414
+
+const validPartTwos = parsed.filter(({ first, second, char, password }) => {
+  const a = password[first - 1] === char;
+  const b = password[second - 1] === char;
+
+  return a !== b;
+});
+
+const partTwo = validPartTwos.length;
+console.log("part two", partTwo); // 413
---
+++ 2020/day02/original.js
@@ -0,0 +1,32 @@
+const lodash = require("lodash");
+const readFile = require("../../helpers/readFile");
+
+// 4-5 h: hhhhhh
+const input = readFile(__dirname, "./input.txt");
+
+let z = 0;
+input.forEach(line => {
+  const [a, str] = line.split(": ");
+  const [range, char] = a.split(" ");
+  const [from, to] = range.split("-").map(x => parseInt(x));
+
+  const count = lodash.countBy(str)[char];
+  if (count >= from && count <= to) ++z;
+});
+const partOne = z;
+console.log("part one", partOne); // 414
+
+let zz = 0;
+input.forEach(line => {
+  const [a, str] = line.split(": ");
+  const [range, char] = a.split(" ");
+  const [from, to] = range.split("-").map(x => parseInt(x));
+
+  const m = str[from - 1] === char;
+  const n = str[to - 1] === char;
+
+  if (m !== n) ++zz;
+});
+
+const partTwo = zz;
+console.log("part two", partTwo); // 413
