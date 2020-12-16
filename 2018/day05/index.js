fd65c2b8dadb0892f0aa405459d7877a132ebd2f
Matt R. Wilson <mwilson@shutterstock.com>
2020-12-16 09:51:07 -0700

    2018 Day 5

---
+++ 2018/day05/index.js
@@ -0,0 +1,28 @@
+const readFile = require("../../helpers/readFile");
+
+const input = readFile(__dirname, "./input.txt")[0];
+
+const sameButDiff = (a, b) => a !== b && a.toUpperCase() === b.toUpperCase();
+
+function reactPolymer(polymer) {
+  const buf = [];
+  for (const char of polymer) {
+    if (buf.length && sameButDiff(char, buf[buf.length - 1])) {
+      buf.pop();
+    } else {
+      buf.push(char);
+    }
+  }
+  return buf.length;
+}
+
+const partOne = reactPolymer(input);
+console.log("part one", partOne); // 10766
+
+const units = new Set(input.toLowerCase());
+const lengths = [...units].map((unit) =>
+  reactPolymer(input.replace(new RegExp(unit, "ig"), ""))
+);
+
+const partTwo = Math.min(...lengths);
+console.log("part two", partTwo); // 6538
